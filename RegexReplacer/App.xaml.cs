e7103502fe82e55aea5b5c21444e2e62b70ba123
Emilien NATTER <enatter@knowmore.fr>
2017-06-30 19:16:21 +0200

    Parse args

--- RegexReplacer/App.xaml.cs
+++ RegexReplacer/App.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Windows;
+using NDesk.Options;
 
 namespace RegexReplacer
 {
@@ -12,5 +9,44 @@ namespace RegexReplacer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string filePattern;
+            string findRegex;
+            string replace;
+
+            var optionSet = new OptionSet
+            {
+                {
+                    "fs|files=",
+                    "",
+                    f => filePattern = f
+                },
+                {
+                    "f|find=",
+                    "",
+                    f => findRegex = f
+                },
+                {
+                    "r|replace=",
+                    "",
+                    f => replace = f
+                }
+            };
+
+
+            try
+            {
+                var extra = optionSet.Parse(e.Args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Try '--help' for more information.");
+                return;
+            }
+        }
     }
 }
